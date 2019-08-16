9d7db87e2552bd6e09c6f55ef39e065395bc62cb
wanghailin1 <wanghailin1@corp.netease.com>
2019-08-17 03:19:40 +0800

    leetcode 2019-08-17 三数之和

--- src/leetcode/array/ThreeSum.java
+++ src/leetcode/array/ThreeSum.java
@@ -20,7 +20,7 @@ public class ThreeSum {
         }
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){
+        for(int i=0;i<nums.length-2;i++) {
             if(i>0 && nums[i-1]==nums[i]){ // 去重
                 continue;
             }
@@ -34,7 +34,15 @@ public class ThreeSum {
                     temp.add(nums[left]);
                     temp.add(nums[right]);
                     result.add(temp);
-                    break;
+                    left++;
+                    while (left < right && nums[left] == nums[left-1]) {
+                        left++;
+                    }
+                    right--;
+                    while(left <right && nums[right]==nums[right+1]){
+                        right--;
+                    }
+
                 } else if(nums[left]+nums[right]<cha){
                     left ++;
                 } else if(nums[left]+nums[right]>cha){
