886b3dd93221ff8ee507fdc2cacc2e8e5f014175
wanghailin1 <wanghailin1@corp.netease.com>
2019-10-17 11:29:40 +0800

    LeetCode

--- src/leetcode/LeetCode199.java
+++ src/leetcode/LeetCode199.java
@@ -1,7 +1,9 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
@@ -34,7 +36,8 @@ public class LeetCode199 {
 
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res=new ArrayList<>();
-        if(root==null) return res;
+        if(root==null)
+            return res;
         Queue<TreeNode> queue=new LinkedList<>();
         queue.offer(root);
         while(!queue.isEmpty()){
