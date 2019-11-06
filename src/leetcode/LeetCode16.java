b447aba167166a5d9da9d2b98fd9faf9bb08f8c4
wanghailin1 <wanghailin1@corp.netease.com>
2019-11-06 17:54:40 +0800

    leetcode16

---
+++ src/leetcode/LeetCode16.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class LeetCode16 {
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums==null || nums.length < 3){
+            return 0;
+        }
+        int result =
+    }
+}
